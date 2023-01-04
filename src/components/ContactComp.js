9d1b39642da6e8c765bf75c680cea55a7af3ba3f
hjvdz <hossvadi@gmail.com>
2023-01-04 16:12:05 +0100

    Box to Card, last modifications

--- src/components/ContactComp.js
+++ src/components/ContactComp.js
@@ -2,7 +2,9 @@ import Row from '@mui/material/Container';
 import Col from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 
-import {Grid, Card, Button, Box} from "@mui/material";
+import {
+    Grid, Card, Box, Button, Typography,
+} from "@mui/material";
 
 const styleObj = {
     paddingTop: "50px",
@@ -15,35 +17,37 @@ export default function ContactComp(props) {
         <Grid container fluid>
             <Row>
                 <Col>
-                <p style={styleObj}>Mobile phone: +123456789</p>
+                    <Card style={{ padding: "15px", marginTop: "25px", marginBottom: "5px"}}>
+                        <Typography variant="h5" gutterBottom style={styleObj}>Mobile phone: +123456789</Typography>
+                        
+                        <p>Our live chat is now: <b>closed</b></p>
+                        <Box display="flex" justifyContent="center" alignItems="center">
+                            <Button style={{marginTop: "20px"}} variant="contained" color="primary" size='large'>Chat with us</Button>
+                        </Box>
 
-                <p>Our live chat is now: <b>closed</b></p>
-                <Box display="flex" justifyContent="center" alignItems="center">
-                    <Button style={{marginTop: "20px"}} variant="contained" color="primary" size='large'>Chat with us</Button>
-                </Box>
-
-                <Card display="flex" justifyContent="center" alignItems="center" style={{padding: "20px", marginTop: "30px", marginBottom: "30px", backgroundColor: "#eee"}} sx={{ border: 1, borderRadius: '16px' }}>
+                        <Card display="flex" justifyContent="center" alignItems="center" style={{padding: "20px", marginTop: "30px", marginBottom: "30px", backgroundColor: "#eee"}} sx={{ border: 1, borderRadius: '16px' }}>
                 
-                <form style = {{marginTop: "20px"}}>
-                    <p><b>Send us an email</b></p>
-                    <Card component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}} noValidate autoComplete="off">
-                    <div>
-                        <TextField id="filled-multiline-flexible" label="Email" multiline maxRows={4} variant="filled"/>
+                        <form style = {{marginTop: "20px"}}>
+                        <p><b>Send us an email</b></p>
+                        <Card component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}} noValidate autoComplete="off">
+                        <div>
+                            <TextField id="filled-multiline-flexible" label="Email" multiline maxRows={4} variant="filled"/>
 
-                        <TextField id="filled-multiline-static" label="Message" multiline rows={4} defaultValue="Your message" variant="filled"/>
-                    </div>
+                            <TextField id="filled-multiline-static" label="Message" multiline rows={4} defaultValue="Your message" variant="filled"/>
+                        </div>
+                        </Card>
+
+                        <Box display="flex" justifyContent="center" alignItems="center">
+                            <Button style={{marginTop: "20px"}} variant="contained" color="primary" size='large'>Submit</Button>
+                        </Box>
+                        </form>
+                        </Card>
                     </Card>
-
-                    <Box display="flex" justifyContent="center" alignItems="center">
-                        <Button style={{marginTop: "20px"}} variant="contained" color="primary" size='large'>Submit</Button>
-                    </Box>
-                </form>
-                </Card>
-
                 </Col>
             </Row>
         </Grid>
     );
 }
+
 
 export {ContactComp}
--- src/components/HelpComp.js
+++ src/components/HelpComp.js
@@ -1,14 +1,15 @@
 import Row from '@mui/material/Container';
 import Col from '@mui/material/Container';
 
-import {Grid, Box, Button, Typography} from "@mui/material";
-
+import {
+    Grid, Card, Box, Button, Typography,
+} from "@mui/material";
 
 const styleObj = {
     fontSize: 35,
     color: "#000",
-    paddingTop: "50px",
-    paddingBottom: "20px",
+    paddingTop: "25px",
+    paddingBottom: "30px",
 }
 
 const styleObjBold = {
@@ -24,21 +25,24 @@ export default function HelpComp(props) {
         <Grid container fluid>
             <Row>
                 <Col>
-                <Typography variant="h3" gutterBottom style={styleObj}>FAQ</Typography>
-                
-                <Typography variant="h5" gutterBottom style={styleObjBold}>Question 1</Typography>
-                <Typography variant="body1" gutterBottom><b>Answers:</b> Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica with ranging across all continents except Antarctica</Typography>
-                
-                <Typography variant="h5" gutterBottom style={styleObjBold}>Question 2</Typography>
-                <Typography variant="body1" gutterBottom><b>Answers:</b> Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica with ranging across all continents except Antarctica</Typography>
+                    <Card style={{ padding: "15px", marginTop: "25px", marginBottom: "5px"}}>
+                        <Typography variant="h3" gutterBottom style={styleObj}>FAQ</Typography>
+                        <hr/>
 
-                <Typography variant="h5" gutterBottom style={styleObjBold}>Question 3</Typography>
-                <Typography variant="body1" gutterBottom><b>Answers:</b> Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica with ranging across all continents except Antarctica</Typography>
+                        <Typography variant="h5" gutterBottom style={styleObjBold}>Question 1</Typography>
+                        <Typography variant="body1" gutterBottom><b>Answers:</b> Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica with ranging across all continents except Antarctica</Typography>
+                        
+                        <Typography variant="h5" gutterBottom style={styleObjBold}>Question 2</Typography>
+                        <Typography variant="body1" gutterBottom><b>Answers:</b> Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica with ranging across all continents except Antarctica</Typography>
+
+                        <Typography variant="h5" gutterBottom style={styleObjBold}>Question 3</Typography>
+                        <Typography variant="body1" gutterBottom><b>Answers:</b> Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica with ranging across all continents except Antarctica</Typography>
+
+                        <Box m={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
+                            <Button style={{marginTop: "50px"}} variant="contained" color="primary" size='large' onClick={()=> window.location.href='/About/Contact'}>Contact us</Button>
+                        </Box>
+                    </Card>
                 </Col>
-
-                <Box m={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
-                    <Button style={{marginTop: "50px"}} variant="contained" color="primary" size='large' onClick={()=> window.location.href='/About/Contact'}>Contact us</Button>
-                </Box>
             </Row>
         </Grid>
     );
